d6312a1de6c3d558d1becbd8707abcc697776120
ssy4230@naver.com <ssy4230@naver.com>
2021-02-06 03:47:29 +0900

    supertest

--- nodebird/app.js
+++ nodebird/app.js
@@ -64,12 +64,11 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
+  console.error(err);
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
   res.render("error");
 });
 
-app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기중");
-});
+module.exports = app;
